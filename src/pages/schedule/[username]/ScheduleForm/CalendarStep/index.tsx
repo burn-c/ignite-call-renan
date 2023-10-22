f8413bbe112c141cb31c8900d2dae60fe6b2b4a0
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-22 13:04:54 -0300

    feat(comp): adds CalendarStep

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,40 @@
+import { Calendar } from '@/components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerList,
+  TimerPickerItem,
+} from './styles'
+
+export default function CalendarStep() {
+  const isDateSelected = true
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimerPickerItem>08:00</TimerPickerItem>
+            <TimerPickerItem>09:00</TimerPickerItem>
+            <TimerPickerItem>10:00</TimerPickerItem>
+            <TimerPickerItem>11:00</TimerPickerItem>
+            <TimerPickerItem>12:00</TimerPickerItem>
+            <TimerPickerItem>13:00</TimerPickerItem>
+            <TimerPickerItem>14:00</TimerPickerItem>
+            <TimerPickerItem>15:00</TimerPickerItem>
+            <TimerPickerItem>16:00</TimerPickerItem>
+            <TimerPickerItem>17:00</TimerPickerItem>
+            <TimerPickerItem>18:00</TimerPickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
