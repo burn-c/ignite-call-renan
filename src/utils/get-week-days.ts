272149f5b77534b4f1a915e7ae8c7f7294352f3e
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-22 12:17:53 -0300

    feat(getWeekDays): adds short params

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,13 +1,20 @@
-function captalize(string: string) {
-  return string[0].toUpperCase() + string.substring(1)
+function captalize(string: string, numberOfLetters: number) {
+  return (
+    string.substring(0, numberOfLetters).toUpperCase() +
+    string.substring(numberOfLetters + 1)
+  )
 }
 
-export function getWeekDays() {
+export function getWeekDays(short = false) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   const arrayDays = Array.from(Array(7).keys()).map(
     (day) => formatter.format(new Date(Date.UTC(2021, 5, day))), // Data precisa iniciar no primeiro dia da semana ( Domingo)
   )
 
-  return arrayDays.map((day) => captalize(day))
+  if (short) {
+    return arrayDays.map((day) => captalize(day, 3).substring(0, 3))
+  }
+
+  return arrayDays.map((day) => captalize(day, 1))
 }
