58ee8aa81789c205d99d0742697138de133edb18
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-23 22:55:52 -0300

    lib: adds next-seo and global setup

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,11 +2,11 @@ import '@lib/dayjs'
 
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import { globalStyles } from '@/styles/global'
-import dayjs from 'dayjs'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -17,6 +17,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: process.env.APP_URL,
+            siteName: 'Bunr Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
